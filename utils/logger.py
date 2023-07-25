1fcfa4b8733fe0b683d71cd446a192f4a0cdc5b1
Matias Grin <mgrin@frba.utn.edu.ar>
2023-07-24 21:13:44 -0300

    Cambio logger

--- utils/logger.py
+++ utils/logger.py
@@ -4,7 +4,7 @@ logname = 'logger.txt'
 
 logging.basicConfig(filename=logname,
                     filemode='a',
-                    format='%(asctime)s,%(msecs)d %(levelname)s - %(message)s',
+                    format='[%(asctime)s] %(levelname)s - %(message)s',
                     datefmt='%H:%M:%S',
                     level=logging.DEBUG)
 
